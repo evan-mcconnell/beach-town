420aa8fa064edc8037ac2b9a39fecc397ab519af
Kimberly McConnell and Victoria Martinez <example@example.com>
2019-03-04 15:14:18 -0800

    add animation library

--- src/index.js
+++ src/index.js
@@ -1,3 +1,67 @@
 import './sass/styles.scss';
+import './letteranimation.min.js';
 
-console.log("hi");
+// window.onload = function() {
+//     animateSequence();
+//     animateRandom();
+// };
+//
+// function animateSequence() {
+//   console.log("bye");
+//     var a = document.getElementsByClassName('sequence');
+//     for (var i = 0; i < a.length; i++) {
+//         var $this = a[i];
+//         var letter = $this.innerHTML;
+//         letter = letter.trim();
+//         var str = '';
+//         var delay = 100;
+//         for (l = 0; l < letter.length; l++) {
+//             if (letter[l] != ' ') {
+//                 str += '<span style="animation-delay:' + delay + 'ms; -moz-animation-delay:' + delay + 'ms; -webkit-animation-delay:' + delay + 'ms; ">' + letter[l] + '</span>';
+//                 delay += 150;
+//             } else
+//                 str += letter[l];
+//         }
+//         $this.innerHTML = str;
+//     }
+// }
+//
+// function animateRandom() {
+//   console.log("hi");
+//     var a = document.getElementsByClassName('random');
+//     for (var i = 0; i < a.length; i++) {
+//         var $this = a[i];
+//         var letter = $this.innerHTML;
+//         letter = letter.trim();
+//         var delay = 70;
+//         var delayArray = new Array;
+//         var randLetter = new Array;
+//         var j = 0;
+//         for (j = 0; j < letter.length; j++) {
+//             while (1) {
+//                 var random = getRandomInt(0, (letter.length - 1));
+//                 if (delayArray.indexOf(random) == -1)
+//                     break;
+//             }
+//             delayArray[j] = random;
+//         }
+//         console.log("3");
+//         for (l = 0; l < delayArray.length; l++) {
+//             var str = '';
+//             var index = delayArray[l];
+//             if (letter[index] != ' ') {
+//                 str = '<span style="animation-delay:' + delay + 'ms; -moz-animation-delay:' + delay + 'ms; -webkit-animation-delay:' + delay + 'ms; ">' + letter[index] + '</span>';
+//                 randLetter[index] = str;
+//             } else
+//                 randLetter[index] = letter[index];
+//             delay += 80;
+//         }
+//         console.log("4");
+//         randLetter = randLetter.join("");
+//         $this.innerHTML = randLetter;
+//     }
+// }
+//
+// function getRandomInt(min, max) {
+//     return Math.floor(Math.random() * (max - min + 1)) + min;
+// }
